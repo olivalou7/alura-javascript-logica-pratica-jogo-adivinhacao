12541ffc4c593732def48e35ec83a3bb868db5d1
luis <l254968@hotmail.com>
2025-12-02 11:41:45 -0300

    criada função em javascript para exibir texto na tela (exibir_texto_na_tela)

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 let numero_secreto = parseInt(Math.random() * 10 + 1);
 console.log(numero_secreto)
 
+function exibir_mensagem_na_tela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibir_mensagem_na_tela(".mensagem-titulo", "Jogo de Adivinhação");
+exibir_mensagem_na_tela(".mensagem-paragrafo", "Informe um número");
+
 function verificar_acerto() {
     let chute = document.querySelector('.chute').value;
 
     if (chute == numero_secreto) {
-        document.querySelector('.mensagem-titulo').innerHTML = "Acertou!";
-        document.querySelector('.mensagem-paragrafo').innerHTML = "Você conseguiu em 'x' tentativas";
+        exibir_mensagem_na_tela(".mensagem-titulo", "Acertou!");
+        exibir_mensagem_na_tela(".mensagem-paragrafo", "Você conseguiu em 'x' tentativas");
     } else {
         if (chute > numero_secreto) {
-            document.querySelector('.mensagem-paragrafo').innerHTML = "O número secreto é menor!";
+            exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é menor!");
         } else {
-            document.querySelector('.mensagem-paragrafo').innerHTML = "O número secreto é maior!";
+            exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é maior!");;
         }
     }
 }
