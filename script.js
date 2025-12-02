a63b6b2367068d4f35cfe4c6ad009d3831229537
luis <l254968@hotmail.com>
2025-12-02 14:44:11 -0300

    -adicionado validação da entrada do usuário -adicionado quantidade número de tentativas

--- script.js
+++ script.js
@@ -9,17 +9,30 @@ function exibir_mensagem_na_tela(tag, texto) {
 exibir_mensagem_na_tela(".mensagem-titulo", "Jogo de Adivinhação");
 exibir_mensagem_na_tela(".mensagem-paragrafo", "Informe um número");
 
+let quantidade_tentativas = 1;
+
 function verificar_acerto() {
     let chute = document.querySelector('.chute').value;
 
-    if (chute == numero_secreto) {
-        exibir_mensagem_na_tela(".mensagem-titulo", "Acertou!");
-        exibir_mensagem_na_tela(".mensagem-paragrafo", "Você conseguiu em 'x' tentativas");
+    let validacao_entrada_usuario = chute >= 1 && chute <= 10;
+
+    let palavra_tentativa = quantidade_tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagem_vitoria = `Você conseguiu em ${quantidade_tentativas} ${palavra_tentativa}`;
+
+    if (!validacao_entrada_usuario) {
+        alert("Informe um número de 1 a 10");
+        return;
+    } else {
+        if (chute == numero_secreto) {
+            exibir_mensagem_na_tela(".mensagem-titulo", "Acertou!");
+            exibir_mensagem_na_tela(".mensagem-paragrafo", mensagem_vitoria);
     } else {
         if (chute > numero_secreto) {
             exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é menor!");
         } else {
-            exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é maior!");;
+            exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é maior!");
         }
     }
+    }
+    quantidade_tentativas++;
 }
