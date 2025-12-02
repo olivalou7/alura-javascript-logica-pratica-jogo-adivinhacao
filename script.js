d76ac04f1ec495d5f422d4af5fce8d543ec92a6e
luis <l254968@hotmail.com>
2025-12-02 15:36:40 -0300

    1- Inserir botão 'reiniciar' no jogo; 2- Mudança na função 'gerar_numero_aleatorio()'.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-let numero_secreto = parseInt(Math.random() * 10 + 1);
-console.log(numero_secreto)
+const maximo_numeros_jogo = 10;
+
+function gerar_numero_aleatorio() {
+    return parseInt(Math.random() * maximo_numeros_jogo + 1);
+    //console.log(numero_secreto)
+}
 
 function exibir_mensagem_na_tela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibir_mensagem_na_tela(".mensagem-titulo", "Jogo de Adivinhação");
-exibir_mensagem_na_tela(".mensagem-paragrafo", "Informe um número");
+function exibir_mensagem_inicial() {
+    exibir_mensagem_na_tela(".mensagem-titulo", "Jogo de Adivinhação");
+    exibir_mensagem_na_tela(".mensagem-paragrafo", "Informe um número");
+}
+
+exibir_mensagem_inicial();
+
+let numero_secreto = gerar_numero_aleatorio();
 
 let quantidade_tentativas = 1;
 
 function verificar_acerto() {
     let chute = document.querySelector('.chute').value;
 
-    let validacao_entrada_usuario = chute >= 1 && chute <= 10;
+    let validacao_entrada_usuario = chute >= 1 && chute <= maximo_numeros_jogo;
 
     let palavra_tentativa = quantidade_tentativas > 1 ? "tentativas" : "tentativa";
     let mensagem_vitoria = `Você conseguiu em ${quantidade_tentativas} ${palavra_tentativa}`;
@@ -26,6 +36,8 @@ function verificar_acerto() {
         if (chute == numero_secreto) {
             exibir_mensagem_na_tela(".mensagem-titulo", "Acertou!");
             exibir_mensagem_na_tela(".mensagem-paragrafo", mensagem_vitoria);
+            document.querySelector('#reiniciar').removeAttribute('disabled');
+            document.querySelector("button").setAttribute('disabled', true);
     } else {
         if (chute > numero_secreto) {
             exibir_mensagem_na_tela(".mensagem-paragrafo", "O número secreto é menor!");
@@ -35,4 +47,13 @@ function verificar_acerto() {
     }
     }
     quantidade_tentativas++;
+}
+
+function reiniciar_jogo() {
+    exibir_mensagem_inicial();
+    numero_secreto = gerar_numero_aleatorio();
+    tentativas = 1;
+    document.querySelector('input').value = '';
+    document.querySelector('#reiniciar').setAttribute('disabled', true);
+    document.querySelector('button').removeAttribute('disabled');
 }
